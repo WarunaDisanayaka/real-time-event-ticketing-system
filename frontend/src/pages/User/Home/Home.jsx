23c6f578be83e51b3023f8c38ae3d49d77115e90
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-11-10 20:22:22 +0530

    Added footer component

--- frontend/src/pages/User/Home/Home.jsx
+++ frontend/src/pages/User/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Footer from '../../../components/User/Footer/Footer'
 import Navigation from '../../../components/User/Navigation/Navigation'
 
 const Home = () => {
     return (
         <>
             <Navigation />
+            <Footer />
         </>
     )
 }
